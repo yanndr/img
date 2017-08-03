a867ff847bc3193dd2e3da5cc49b7790540b3f22
Yann <yann.druffin@gmail.com>
2017-08-03 14:48:18 -0700

    Separeated cmd and lib

---
+++ cmd/imgresize/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"path/filepath"
+	"sync"
+
+	"github.com/yanndr/imgresize"
+)
+
+func main() {
+	sizePtr := flag.Int("s", 50, "Size in % of the original image.")
+
+	flag.Parse()
+
+	var files []string
+	if len(flag.Args()) < 2 {
+		input := "./*.*"
+		var err error
+		files, err = filepath.Glob(input)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+	} else {
+		for i := 1; i < len(flag.Args()); i++ {
+			files = append(files, flag.Args()[i])
+		}
+	}
+
+	var wg sync.WaitGroup
+	for _, f := range files {
+		wg.Add(1)
+		go func(f string) {
+			defer wg.Done()
+			err := imgresize.ResizeImg(f, *sizePtr)
+			if err != nil {
+				fmt.Println("Error with ", f, ": ", err)
+				return
+			}
+
+			fmt.Println(f, "-> Processed.")
+		}(f)
+
+	}
+
+	wg.Wait()
+	fmt.Println("done")
+
+	fmt.Println(`
+	 _[_]_  
+          (")  
+       --( : )--'
+        (  :  )
+      "" -...-'""`)
+
+}
--- imgresize.go
+++ imgresize.go
@@ -1,71 +1,18 @@
-package main
+package imgresize
 
 import (
 	"errors"
-	"flag"
 	"fmt"
 	"image"
 	"image/jpeg"
 	"image/png"
 	"io"
-	"log"
 	"os"
 	"path"
-	"path/filepath"
 	"strings"
-	"sync"
 
 	"github.com/nfnt/resize"
 )
-
-func main() {
-	sizePtr := flag.Int("s", 50, "Size in % of the original image.")
-
-	flag.Parse()
-
-	var files []string
-	if len(flag.Args()) < 2 {
-		input := "./*.*"
-		var err error
-		files, err = filepath.Glob(input)
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
-
-	} else {
-		for i := 1; i < len(flag.Args()); i++ {
-			files = append(files, flag.Args()[i])
-		}
-	}
-
-	var wg sync.WaitGroup
-	for _, f := range files {
-		wg.Add(1)
-		go func(f string) {
-			defer wg.Done()
-			err := resizeImg(f, *sizePtr)
-			if err != nil {
-				fmt.Println("Error with ", f, ": ", err)
-				return
-			}
-
-			fmt.Println(f, "-> Processed.")
-		}(f)
-
-	}
-
-	wg.Wait()
-	fmt.Println("done")
-
-	fmt.Println(`
-	 _[_]_  
-          (")  
-       --( : )--'
-        (  :  )
-      "" -...-'""`)
-
-}
 
 func decode(extenstion string, r io.Reader) (image.Image, error) {
 	ext := strings.ToLower(extenstion)
@@ -91,7 +38,7 @@ func encode(extenstion string, w io.Writer, m image.Image) error {
 	return errors.New("Doesn't seems to be an image")
 }
 
-func resizeImg(f string, size int) error {
+func ResizeImg(f string, size int) error {
 
 	file, err := os.Open(f)
 	if err != nil {
